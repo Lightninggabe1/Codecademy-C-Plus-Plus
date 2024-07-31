a8363d78d0495a64d9aac1a45d647364e2ff498e
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-31 10:51:33 +0800

    Update COMPILE & EXECUTE.cpp
    //Compile and Execute: 2/5

--- COMPILE & EXECUTE.cpp
+++ COMPILE & EXECUTE.cpp
@@ -1,2 +1,19 @@
 //1/5
 //Code → Save → Compile → Execute
+
+//2/5
+//Compile and Execute
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Hello World!\n";
+  
+}
+//Compiling the program: translating what we write into machine code
+//$ g++ hello.cpp
+//Executing the program: This, like compiling, is also done  by the run button
+//$ ./a.out
+//Hello World!
+//$ 
