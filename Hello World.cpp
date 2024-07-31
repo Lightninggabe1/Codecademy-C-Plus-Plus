7e5b0104c6235879b8561cfcfb231539020d1465
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-31 10:35:57 +0800

    Create Hello World.cpp
    //Hello World module.

---
+++ Hello World.cpp
@@ -0,0 +1,48 @@
+//1/5
+
+//2/5
+#include <iostream>
+
+int main() {
+  
+  std::cout << "Hello World!\n";
+
+}
+
+//3/5
+#include <iostream>
+
+int main() {
+  
+  std::cout << "Codecademy\n";
+  
+}
+
+//4/5
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "        1\n";
+  std::cout << "      2 3\n";
+  std::cout << "    4 5 6\n";
+  std::cout << " 7 8 9 10\n";
+}
+
+//5/5
+//letter.cpp
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << " \n";
+  std::cout << "Goals:\n";
+  std::cout << "• Learn C++\n";
+  std::cout << "• Build a working program in GitHUb or Replit using C++\n"; 
+  std::cout << "• Build a text adventure game using C++ in GitHub or Replit\n";
+  std::cout << " \n";
+  std::cout << "Gabriel Chan, 30/7/2024";
+}
