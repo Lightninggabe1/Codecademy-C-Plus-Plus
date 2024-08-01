51578f52f1ec8adfc62814fc2e83ebc40c74aa26
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-01 11:07:16 +0800

    Create LEARN C++.cpp
    1 2
    3
    45
    AAAA
    A
    1
    2
    4
    5
    E
    E
    E
    E
    E E
    E I
    B
    B
    B
    B
    B
    B
    B
    2
    3
    B
    B
    B B
    B
    B
    45
    B
    B
    B
    B
    B
    B
    1
    2
    5
    3
    4
    FFFFF
    F
    F
    FF F
    1
    2 345
    CC C
    C
    C
    C
    C
    CC C
    1
    2 3
    GGG
    4 5
    G
    G
    G
    G
    G
    GGGGG
    G
    G
    GGG
    2
    3 45
    D
    D D
    D
    D
    D
    D
    D
    D
    D D
    D D
    D
    D
    D
    D
    D
    1 2 345
    H

---
+++ LEARN C++.cpp
@@ -0,0 +1,13 @@
+//I used to use Replit
+#include <iostream>
+
+int main() {
+  
+  std::cout << " GGG    CCC \n";
+  std::cout << "G   G  C   C\n";
+  std::cout << "G      C    \n";
+  std::cout << "GGGGG  C    \n";
+  std::cout << "G   G  C    \n";
+  std::cout << "G   G  C   C\n";
+  std::cout << " GGG    CCC\n";
+}
