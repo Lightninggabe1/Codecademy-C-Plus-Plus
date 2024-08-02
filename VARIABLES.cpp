f1fa3a93f2fbd7958b46e8ba2e6d55a72f416cda
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-02 10:05:30 +0800

    Update VARIABLES.cpp
    //Challenge: Temperature (Part 2) 9/10

--- VARIABLES.cpp
+++ VARIABLES.cpp
@@ -100,3 +100,22 @@ int main() {
   
   
 }
+
+//9/10
+//Challenge: Temperature (Part 2)
+#include <iostream>
+
+int main() {
+  
+  double tempf;
+  double tempc;
+  
+  // Ask the user
+  std::cout << "Enter the temperature in Fahrenheit: ";
+  std::cin >> tempf;
+  
+  tempc = (tempf - 32) / 1.8;
+  
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+  
+}
