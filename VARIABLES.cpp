979af5fc8008fd66b537b87dc9e205241f74851b
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-02 09:40:41 +0800

    Update VARIABLES.cpp
    //Arithmetic Operators 5/10

--- VARIABLES.cpp
+++ VARIABLES.cpp
@@ -41,3 +41,19 @@ int main() {
    return 0;
    
 }
+
+//5/10
+//Arithmetic Operators
+#include <iostream>
+
+int main() {
+
+  int score = 0;
+
+  // Change score here:
+  score = 1234*99; 
+  std::cout << score << "\n";
+  
+
+
+}
