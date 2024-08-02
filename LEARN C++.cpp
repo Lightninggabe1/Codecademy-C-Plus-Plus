9722b2bed6a052df45d82d07b1f29590fa0500de
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-02 10:29:23 +0800

    Update LEARN C++.cpp
    🎯Objective
    LEARN C++
    Dog Years

--- LEARN C++.cpp
+++ LEARN C++.cpp
@@ -1,3 +1,4 @@
+//FIrst Objective:
 //I used to use Replit
 #include <iostream>
 
@@ -10,4 +11,20 @@ int main() {
   std::cout << "G   G  C    \n";
   std::cout << "G   G  C   C\n";
   std::cout << " GGG    CCC\n";
+}
+
+
+//Dog Years (after VARIABLES.cpp)
+#include <iostream>
+
+int main() {
+  //this is the age they asked me to put
+  int dog_age = 6;
+  int early_years,later_years,human_years;
+  early_years = 21;
+  //for the later years
+  later_years=(dog_age-2)*4;
+  //human years
+  human_years = early_years + later_years;
+  std::cout << "My name is Dog! Ruff ruff, I am " << human_years << " years old in human years.";
 }
