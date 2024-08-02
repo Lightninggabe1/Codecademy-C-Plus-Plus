2f46e2b6ac998bfdd51b9d10f801026ceb77bd2a
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-02 09:48:31 +0800

    Update VARIABLES.cpp
    //User Input 7/10

--- VARIABLES.cpp
+++ VARIABLES.cpp
@@ -70,3 +70,18 @@ int main() {
   std::cout << "Player score: " << score << "\n";
    
 }
+
+//7/10
+//User Input
+#include <iostream>
+
+int main() {
+  
+  int tip = 0;
+  
+  std::cout << "Enter tip amount: ";
+  std::cin >> tip;
+  std::cout << "You paid " << tip << " dollars\n";
+  
+  
+}
