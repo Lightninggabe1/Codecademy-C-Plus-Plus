fe912c4c9b1295773015d9adcc05cb6d2b4165f6
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-04 09:58:37 +0800

    Update CONDITIONALS & LOGIC.cpp
    //Coin Flip Demo 2/8

--- CONDITIONALS & LOGIC.cpp
+++ CONDITIONALS & LOGIC.cpp
@@ -1,2 +1,31 @@
 //1/8
 //Introduction to Conditionals & Logic
+
+//2/8
+//Coin Flip Demo (only a demo) 
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
+int main() {
+  
+  // Create a number that's 0 or 1
+  
+  srand (time(NULL));
+  int	coin = rand() % 2;
+  
+  // If number is 0: Heads
+  // If it is not 0: Tails
+  
+  if (coin == 0) {
+  
+    std::cout << "Heads\n";
+  
+  }
+	else {
+	
+    std::cout << "Tails\n";
+  
+  }
+  
+}
