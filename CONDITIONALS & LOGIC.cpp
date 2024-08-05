a1280e6834958f2560f009536910d275af1699f5
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-05 10:24:26 +0800

    Update CONDITIONALS & LOGIC.cpp
    //Relational Operators 4/8

--- CONDITIONALS & LOGIC.cpp
+++ CONDITIONALS & LOGIC.cpp
@@ -44,3 +44,28 @@ int main() {
   
   
 }
+
+//4/8
+//Relational Operators
+//When writing conditional statements, sometimes we need to use different types of operators to compare values. These operators are called relational operators.
+//To have a condition, we need relational operators:
+//== equal to
+//!= not equal to
+//> greater than
+//< less than
+//>= greater than or equal to
+//<= less than or equal to
+//Relational operators compare the value on the left with the value on the right.
+#include <iostream>
+
+int main() {
+
+  int grade = 90;
+  
+  if (grade != 60) {
+  
+    std::cout << "Pass\n";
+  
+  }
+  
+}
