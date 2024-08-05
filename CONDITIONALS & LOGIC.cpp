1b5601e6d098243668ed77336bcf06e2b1e26112
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-05 10:46:35 +0800

    Update CONDITIONALS & LOGIC.cpp
    //Review 8/8

--- CONDITIONALS & LOGIC.cpp
+++ CONDITIONALS & LOGIC.cpp
@@ -159,3 +159,41 @@ int main() {
   }
   
 }
+
+//8/8
+//Review
+#include <iostream>
+
+int main() {
+  double weight;
+  int planet;
+  std::cout << "Enter your Earth weight: \n";
+  std::cin >> weight;
+  std::cout << "\n";
+  std::cout << "Enter the number of the planet you wish to fight on: \n";
+  std::cin >> planet;
+  switch (planet) {
+    case 1 :
+      weight = weight * 0.38;
+      break;
+    case 2 :
+      weight = weight * 0.91;
+      break;
+    case 3 :
+      weight = weight * 0.38;
+      break;
+    case 4 :
+      weight = weight * 2.34;
+      break;
+    case 5 :
+      weight = weight * 1.06;
+      break;
+    case 6 :
+      weight = weight * 0.92;
+      break;
+    case 7 :
+      weight = weight * 1.19;
+      break;
+  }
+  std::cout << "Your weight is " << weight << "\n";
+}
