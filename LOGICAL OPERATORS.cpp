f821f149d5bd3ccba37f36a3457563bd47fed1d0
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-06 20:32:45 +0800

    Update LOGICAL OPERATORS.cpp
    //Logical Operators: ! 4/5

--- LOGICAL OPERATORS.cpp
+++ LOGICAL OPERATORS.cpp
@@ -31,3 +31,18 @@ int main() {
   }
 
 }
+
+//4/5
+//Logical Operators: !
+#include <iostream>
+
+int main() {
+  
+  bool logged_in = false;
+  
+  // Write the code below:
+  if (!logged_in) {
+    std::cout << "Try again";
+  }
+  
+}
