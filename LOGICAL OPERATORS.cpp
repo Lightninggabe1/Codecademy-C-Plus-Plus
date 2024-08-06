b520e0a0f4779f77ab1906c955734e2ddd8450e5
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-06 20:20:40 +0800

    Create LOGICAL OPERATORS.cpp
    //Introduction to Logical Operators 1/5
    //Logical Operator: && 2/5

---
+++ LOGICAL OPERATORS.cpp
@@ -0,0 +1,18 @@
+//1/5
+//Introduction to Logical Operators
+
+//2/5
+//Logical Operator: &&
+#include <iostream>
+
+int main() {
+  
+  int hunger = true;
+  int anger = true;
+  
+  // Write the code below:
+  if (hunger == true && anger == true) {
+    std::cout << "Hangry";
+  }
+  
+}
