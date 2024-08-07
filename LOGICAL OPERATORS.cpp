2e3947bb5fa1bb842b664c4c827a4b7c56834f37
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-07 13:16:27 +0800

    Update LOGICAL OPERATORS.cpp
    //Review 5/5

--- LOGICAL OPERATORS.cpp
+++ LOGICAL OPERATORS.cpp
@@ -46,3 +46,28 @@ int main() {
   }
   
 }
+
+//5/5
+//Review
+#include <iostream>
+
+int main() {
+  
+  int year = 0;
+  std::cout << "Enter a year: ";
+  std::cin >> year;
+  if (year < 1000 or year > 9999) {
+    std::cout << "Invalid input.\n";
+    }
+  else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+
+      std::cout << year << " is a leap year.\n";
+
+    }
+  else {
+
+      std::cout << year << " is not a leap year.\n";
+
+    }
+  
+}
