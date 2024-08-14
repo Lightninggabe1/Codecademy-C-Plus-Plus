5cdc812bf86b0dc8cf113e57e882cd767fb82f0b
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-14 12:11:42 +0800

    Update LOOPS.cpp
    //99 Bottles 6/7

--- LOOPS.cpp
+++ LOOPS.cpp
@@ -98,4 +98,25 @@ int main() {
   
 }
 
+//99 Bottles 6/7
+#include <iostream>
 
+int main() {
+
+  // Write a for loop here:
+  for (int bottles = 99; bottles > 0;bottles--) {
+    std::cout << bottles << " bottles of pop on the wall.\n";
+    std::cout << "Take one down and pass it around.\n";
+    std::cout << bottles-1 << " bottles of pop on the wall.\n";
+    std::cout << "\n";
+  }
+  std::cout << "No more bottles of pop on the wall.\n";
+  std::cout << "No more bottles of pop.\n";
+  std::cout << "Go to the store and buy some more,\n";
+  std::cout << "99 bottles of pop on the wall.\n";
+  
+  
+    
+  
+  
+}
