4c77cd8ed3df70973adfd82f3309455f4d9c284d
Lightninggabe1 <lightninggabe191@gmail.com>
2024-08-15 09:48:59 +0800

    Update ERRORS.cpp
    //Link-time Errors 3/6

--- ERRORS.cpp
+++ ERRORS.cpp
@@ -27,3 +27,25 @@ int main() {
   }
   
 }
+
+//Link-time Errors 3/6
+#include <iostream>
+
+int main() {
+  
+  int num = 0;
+  int sum = 0;
+  
+  std::cout << "Enter a number: ";
+  std::cin >> num;
+  
+  for (int i = 1; i <= num; i++) {
+    
+    sum = sum + i;
+    std::cout << i << " ";
+    
+  }
+  
+  std::cout << "Sum: " << sum << "\n";
+  
+}
