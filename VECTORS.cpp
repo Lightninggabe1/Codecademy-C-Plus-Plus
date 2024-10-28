586486c26e95caf201c7829bbc3caed6b11a5f01
Lightninggabe1 <lightninggabe191@gmail.com>
2024-10-28 15:48:40 +0800

    Update VECTORS.cpp
    2&3/8

--- VECTORS.cpp
+++ VECTORS.cpp
@@ -1,2 +1,29 @@
 //1/8: Introduction to Vectors
 
+//2/8: Creating a Vector
+#include <iostream>
+#include <vector>
+
+int main() {
+   
+  std::vector<double> subway_adult;
+  
+  // Declare another vector here:
+  std::vector<double> subway_child;
+    
+  
+}
+
+//3/8: Initializing a Vector
+#include <iostream>
+#include <vector>
+
+int main() {
+   
+  std::vector<double> subway_adult = {800, 1200, 1500};
+  
+  // Give subway_child some values:
+  
+  std::vector<double> subway_child = {400, 600, 750};
+
+}
