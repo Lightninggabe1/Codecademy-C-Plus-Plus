4351da53e92e7c54f16b3111ad788d5a752fa8fc
Lightninggabe1 <lightninggabe191@gmail.com>
2024-10-29 10:45:24 +0800

    Update VECTORS.cpp
    5./8

--- VECTORS.cpp
+++ VECTORS.cpp
@@ -43,3 +43,25 @@ int main() {
   
   
 }
+
+//5/8: Adding and Removing Elements 
+#include <iostream>
+#include <vector>
+
+int main() {
+  
+  std::vector<std::string> last_jedi;
+  
+  // Add characters here:
+  last_jedi.push_back("kylo");
+  last_jedi.push_back("rey");
+  last_jedi.push_back("luke");
+  last_jedi.push_back("finn");
+  
+  
+  std::cout << last_jedi[0] << " ";
+  std::cout << last_jedi[1] << " ";
+  std::cout << last_jedi[2] << " ";
+  std::cout << last_jedi[3] << " ";
+  
+}
