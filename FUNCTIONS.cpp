e3ab0c86097acfed93c6f5ef012836d342bdf663
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-01 11:54:47 +0800

    Update FUNCTIONS.cpp with the Final Review

--- FUNCTIONS.cpp
+++ FUNCTIONS.cpp
@@ -124,3 +124,32 @@ int main() {
 }
 
 //9/10: How Parameters & Arguments Work
+
+//10/10: Review of C++ Functions
+#include <iostream>
+
+int main() {
+  
+  // Conduct IT support
+  std::string on_off_attempt;
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+  
+  // Check in with Jenn
+  std::cout << "Oh hi Jen!\n";
+  
+  // Conduct IT support again...
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+
+  // Check in with Roy
+  std::cout << "You stole the stress machine? But that's stealing!\n";
+  
+  // Conduct IT support yet again...zzzz...
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+  
+}
