5b00b3da6cf7c0cfd417497336a204fb48c26fe4
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-04 09:59:49 +0800

    Update CODE CHALLENGE: C++ FUNCTIONS.cpp
    2/7

--- CODE CHALLENGE: C++ FUNCTIONS.cpp
+++ CODE CHALLENGE: C++ FUNCTIONS.cpp
@@ -1,2 +1,15 @@
 //1/7: How It Works
 
+//2/7: Introduction
+#include <iostream>
+
+// Define introduction() here:
+void introduction(std::string first_name, std::string last_name) {
+  std::cout << last_name << ", " << first_name << " " << last_name;
+}
+
+int main() {
+  
+  introduction("Beyonce", "Knowles");
+  
+}
