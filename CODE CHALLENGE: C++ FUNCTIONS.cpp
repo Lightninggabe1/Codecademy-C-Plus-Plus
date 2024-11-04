cf907b4e35a46ac4f792c92ad935d5e139548148
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-04 10:48:11 +0800

    Update CODE CHALLENGE: C++ FUNCTIONS.cpp
    7/7

--- CODE CHALLENGE: C++ FUNCTIONS.cpp
+++ CODE CHALLENGE: C++ FUNCTIONS.cpp
@@ -88,3 +88,26 @@ int main() {
   std::cout << needs_water(10, false) << "\n";
   
 }
+
+//7/7: Palindrome
+#include <iostream>
+
+// Define is_palindrome() here:
+bool is_palindrome(std::string text) {
+  std::string reversedtext = "";
+  for (int i = text.length() - 1; i >= 0;i--) {
+    reversedtext += text[i];
+  } 
+  if (reversedtext == text) {
+    return true;
+  }
+    return false;
+}
+
+int main() {
+  
+  std::cout << is_palindrome("madam") << "\n";
+  std::cout << is_palindrome("ada") << "\n";
+  std::cout << is_palindrome("lovelace") << "\n";
+  
+}
