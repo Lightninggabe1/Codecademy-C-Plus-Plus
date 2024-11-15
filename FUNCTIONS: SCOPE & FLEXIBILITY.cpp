23870ec5c6e0a861ca1fdf4aea7254edcf78b8a9
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-15 10:42:48 +0800

    Update FUNCTIONS: SCOPE & FLEXIBILITY.cpp
    3/9

--- FUNCTIONS: SCOPE & FLEXIBILITY.cpp
+++ FUNCTIONS: SCOPE & FLEXIBILITY.cpp
@@ -48,3 +48,15 @@ bool is_palindrome(std::string text) {
   
   return false;
 }
+
+#include <iostream>
+#include "fns.hpp"
+
+
+int main() {
+  
+  std::cout << is_palindrome("noon") << "\n";
+  std::cout << tenth_power(4) << "\n";
+  std::cout << average(4.0, 7.0) << "\n";
+  
+}
