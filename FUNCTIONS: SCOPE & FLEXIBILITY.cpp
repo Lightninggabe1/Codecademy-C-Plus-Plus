3ff28f89c23ee24cfe1df47e172d428ff537e4b1
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-15 10:40:01 +0800

    Update FUNCTIONS: SCOPE & FLEXIBILITY.cpp
    2/9

--- FUNCTIONS: SCOPE & FLEXIBILITY.cpp
+++ FUNCTIONS: SCOPE & FLEXIBILITY.cpp
@@ -21,3 +21,30 @@ int main() {
   enter_code(0310);
   
 }
+
+//2/9: Multi-File Programs
+#include <iostream>
+#include <cmath>
+
+// Add definitions here:
+double average(double num1, double num2) {
+  return (num1 + num2) / 2;
+}
+
+int tenth_power(int num) {
+  return pow(num, 10);
+}
+
+bool is_palindrome(std::string text) {
+  std::string reversed_text = "";
+  
+  for (int i = text.size() - 1; i >= 0; i--) {
+    reversed_text += text[i];
+  }
+  
+  if (reversed_text == text) {
+    return true;
+  }
+  
+  return false;
+}
