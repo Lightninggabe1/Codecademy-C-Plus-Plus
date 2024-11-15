bf40bf416103c286636e78edfd783ca482348fc4
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-15 10:34:12 +0800

    Create FUNCTIONS: SCOPE & FLEXIBILITY.cpp with 1/9 Preloaded

---
+++ FUNCTIONS: SCOPE & FLEXIBILITY.cpp
@@ -0,0 +1,23 @@
+//1/9: The Scope of Things
+#include <iostream>
+
+
+
+void enter_code(int passcode) {
+  std::string secret_knowledge = "https://content.codecademy.com/courses/regex/onyourexcitingjourneylearningtocodeyouwillfindthis.gif";
+  if (passcode == 0310) {
+    
+    std::cout << secret_knowledge << "\n";
+    
+  } else {
+    
+    std::cout << "Sorry, incorrect!\n";
+    
+  }
+}
+
+int main() {
+  
+  enter_code(0310);
+  
+}
