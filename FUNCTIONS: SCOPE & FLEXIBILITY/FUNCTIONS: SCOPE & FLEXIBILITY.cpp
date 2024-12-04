70b4f7197c88b95140cab8b3358c50db170882b7
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-04 17:09:01 +0800

    Update FUNCTIONS: SCOPE & FLEXIBILITY.cpp
    4/9

--- FUNCTIONS: SCOPE & FLEXIBILITY/FUNCTIONS: SCOPE & FLEXIBILITY.cpp
+++ FUNCTIONS: SCOPE & FLEXIBILITY/FUNCTIONS: SCOPE & FLEXIBILITY.cpp
@@ -60,3 +60,27 @@ int main() {
   std::cout << average(4.0, 7.0) << "\n";
   
 }
+
+//4/9: How to Get Your Functions Inline
+#include <iostream>
+#include <chrono>
+
+#include "night.hpp"
+
+int main() {
+  
+  // Measure time taken for goodnight1():
+  std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+  std::cout << goodnight1("tulip");
+  
+  std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::milli> time_span = end - start;
+
+  // Print time taken for goodnight1():
+  std::cout << "Time taken for goodnight1(): " << time_span.count() << " milliseconds.\n\n";
+  
+  
+  std::cout << goodnight2("eraser", "ivy");
+  
+}
