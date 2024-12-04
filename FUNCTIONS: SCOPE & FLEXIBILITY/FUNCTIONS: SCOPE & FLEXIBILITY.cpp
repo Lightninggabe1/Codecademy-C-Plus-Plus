e1366c4c69c91e74ff9c0ce7f476b9e408c1f67b
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-04 17:35:53 +0800

    Update FUNCTIONS: SCOPE & FLEXIBILITY.cpp
    6/9

--- FUNCTIONS: SCOPE & FLEXIBILITY/FUNCTIONS: SCOPE & FLEXIBILITY.cpp
+++ FUNCTIONS: SCOPE & FLEXIBILITY/FUNCTIONS: SCOPE & FLEXIBILITY.cpp
@@ -105,3 +105,15 @@ int main() {
   std::cout << make_coffee(false, true);
   
 }
+//6/9: Function Overload!
+#include <iostream>
+
+#include "num_ops.hpp"
+
+int main() {
+
+  std::cout << fancy_number(12, 3) << "\n";
+  std::cout << fancy_number(12, 3, 19) << "\n";
+  std::cout << fancy_number(13.5, 3.8) << "\n";
+  
+}
