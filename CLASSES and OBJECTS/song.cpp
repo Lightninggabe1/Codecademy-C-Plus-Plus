38fdbc29d8cd96de775bb225132d7db7ba764664
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-11 11:03:21 +0800

    Update song.cpp
    4/7

--- CLASSES and OBJECTS/song.cpp
+++ CLASSES and OBJECTS/song.cpp
@@ -26,3 +26,31 @@ std::string Song::get_title() {
   return title;
 
 }
+
+//4/7: Access Control: Public and Private
+#include "song.hpp"
+
+// add Song method definitions here:
+void Song::add_title(std::string new_title) {
+
+  title = new_title;
+
+}
+
+std::string Song::get_title() {
+
+  return title;
+
+}
+
+void Song::add_artist(std::string new_artist) {
+
+  artist = new_artist;
+
+}
+
+std::string Song::get_artist() {
+
+  return artist;
+
+}
