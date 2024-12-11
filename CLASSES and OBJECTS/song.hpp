74a855a4708244b1c9033eeea92de86189e8012e
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-11 11:02:39 +0800

    Update song.hpp
    4/7

--- CLASSES and OBJECTS/song.hpp
+++ CLASSES and OBJECTS/song.hpp
@@ -28,3 +28,18 @@ public:
   std::string get_title();
   
 };
+
+//4/7: Access Control: Public and Private
+#include <string>
+
+class Song {
+  
+  std::string title;
+  std::string artist;
+
+public:
+  void add_title(std::string new_title);
+  std::string get_title();
+  void add_artist(std::string new_artist);
+  std::string get_artist();
+};
