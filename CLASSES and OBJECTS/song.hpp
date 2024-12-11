1f330bd4d01a81088ed7d283944a913c9cac4a0d
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-11 10:23:13 +0800

    Update song.hpp

--- CLASSES and OBJECTS/song.hpp
+++ CLASSES and OBJECTS/song.hpp
@@ -3,3 +3,14 @@
 class Song {
   
 };
+
+//2/7: Class Members
+#include <string>
+
+// add the Song class here:
+class Song {
+  std::string title;
+public:
+  void add_title(std::string new_title);
+  std::string get_title();
+};
