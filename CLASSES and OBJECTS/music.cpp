1c769f0eb5b403e5ef4a4f67fade4125ae80c36e
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-11 11:04:07 +0800

    Update music.cpp
    4/7

--- CLASSES and OBJECTS/music.cpp
+++ CLASSES and OBJECTS/music.cpp
@@ -14,7 +14,7 @@ int main() {
 
 }
 
-//3/7 Creating Objects
+//3/7: Creating Objects
 #include <iostream>
 #include "song.hpp"
 
@@ -22,4 +22,15 @@ int main() {
   Song electric_relaxation;
   electric_relaxation.add_title("Electric Relaxation");
   std::cout << electric_relaxation.get_title();
+}
+
+//4/7: Access Control: Public and Private
+#include <iostream>
+#include "song.hpp"
+
+int main() {
+
+  Song electric_relaxation;
+  electric_relaxation.add_artist("A Tribe Called Quest");
+  std::cout << electric_relaxation.get_artist();
 }
