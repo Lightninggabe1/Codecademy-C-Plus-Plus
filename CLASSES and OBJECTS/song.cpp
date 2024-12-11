375b99798e36f29bfabe2978eb84d30fd8632727
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-11 10:37:20 +0800

    Update song.cpp
    3/7

--- CLASSES and OBJECTS/song.cpp
+++ CLASSES and OBJECTS/song.cpp
@@ -10,3 +10,19 @@ void Song::add_title(std::string new_title) {
 std::string Song::get_title() {
   return title;
 }
+
+//3/7: Creating Objects
+#include "song.hpp"
+
+// add Song method definitions here:
+void Song::add_title(std::string new_title) {
+
+  title = new_title;
+
+}
+
+std::string Song::get_title() {
+
+  return title;
+
+}
