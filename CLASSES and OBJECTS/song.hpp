f5f8f93a2a9c7851cbb3e671948cf57ef82c188f
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-11 10:37:50 +0800

    Update song.hpp
    3/7

--- CLASSES and OBJECTS/song.hpp
+++ CLASSES and OBJECTS/song.hpp
@@ -14,3 +14,17 @@ public:
   void add_title(std::string new_title);
   std::string get_title();
 };
+
+//3/7: Creating Objects
+#include <string>
+
+// add the Song class here:
+class Song {
+  
+  std::string title;
+
+public:
+  void add_title(std::string new_title);
+  std::string get_title();
+  
+};
