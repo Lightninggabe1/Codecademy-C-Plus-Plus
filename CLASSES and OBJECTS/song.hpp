f39e951c7404c5caaefb858d5ebb14f603a8e393
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-16 12:37:12 +0800

    Update song.hpp
    6/7

--- CLASSES and OBJECTS/song.hpp
+++ CLASSES and OBJECTS/song.hpp
@@ -57,3 +57,22 @@ public:
   Song(std::string new_title, std::string new_artist);
   
 };
+
+//6/7: Destructors
+#include <string>
+
+class Song {
+  
+  std::string title;
+  std::string artist;
+
+public:
+  Song(std::string new_title, std::string new_artist);
+  // Add a destructor here:
+  ~Song();
+  
+  std::string get_title();
+  
+  std::string get_artist();
+  
+};
