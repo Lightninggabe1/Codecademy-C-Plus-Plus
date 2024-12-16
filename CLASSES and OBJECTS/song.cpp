cdaf34993f5f163e6717247d1b57ab6cdf23d2d4
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-16 11:22:24 +0800

    Update song.cpp
    5/7

--- CLASSES and OBJECTS/song.cpp
+++ CLASSES and OBJECTS/song.cpp
@@ -54,3 +54,24 @@ std::string Song::get_artist() {
   return artist;
 
 }
+
+//5/7: Constructors
+#include "song.hpp"
+
+// add the Song constructor here:
+Song::Song(std::string new_title, std::string new_artist) {
+  title = new_title;
+  artist = new_artist;
+}
+
+std::string Song::get_title() {
+
+  return title;
+
+}
+
+std::string Song::get_artist() {
+
+  return artist;
+
+}
