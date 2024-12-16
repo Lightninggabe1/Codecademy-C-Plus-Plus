e15c59f72a0280bd06402e07dd193d6b80db2cf2
Lightninggabe1 <lightninggabe191@gmail.com>
2024-12-16 12:35:54 +0800

    Update song.cpp
    6/7

--- CLASSES and OBJECTS/song.cpp
+++ CLASSES and OBJECTS/song.cpp
@@ -75,3 +75,29 @@ std::string Song::get_artist() {
   return artist;
 
 }
+
+//6/7: Destructors
+#include "song.hpp"
+#include <iostream>
+
+Song::Song(std::string new_title, std::string new_artist) 
+  : title(new_title), artist(new_artist) {}
+
+// add the Song destructor here:
+Song::~Song() {
+
+  std::cout << "Goodbye " << title << "\n";
+
+}
+
+std::string Song::get_title() {
+
+  return title;
+
+}
+
+std::string Song::get_artist() {
+
+  return artist;
+
+}
